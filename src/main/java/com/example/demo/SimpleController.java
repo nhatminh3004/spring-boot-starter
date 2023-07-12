9fc87696d7c7000603ce2c6045a22f42d2ad2ad6
Nguyễn Lâm Nhật Minh <nhatminhminh57@gmail.com>
2023-07-12 18:40:57 +0700

    change to rest api server ubuntu vps
    change to rest api server ubuntu vps

--- src/main/java/com/example/demo/SimpleController.java
+++ src/main/java/com/example/demo/SimpleController.java
@@ -4,15 +4,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class SimpleController {
-    @Value("${spring.application.name}")
-    String appName;
 
-    @GetMapping("/")
-    public String homePage(Model model) {
-        model.addAttribute("appName", appName);
-        return "home";
+
+    @GetMapping("/api")
+    public String testConnect() {
+
+        return "Connect Success API";
     }
 }
